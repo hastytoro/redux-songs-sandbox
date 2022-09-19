3fa92fe6dbf9cec8f0ef52765805223266ee6712
Luigi Lupini <luigi.lupini@gmail.com>
2022-09-19 17:04:13 +0200

    3) Wiring up the <Provider />

--- src/index.js
+++ src/index.js
@@ -3,4 +3,13 @@ import ReactDOM from "react-dom";
 
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducers from "./reducers";
+
+ReactDOM.render(
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
